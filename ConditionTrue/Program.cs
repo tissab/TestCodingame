8f1339dbbe8a4ac240a3a7444e927bf7a092fb67
Bassit Diby <josuebassit@gmail.com>
2023-01-13 21:20:18 +0100

    MaxPro

--- ConditionTrue/Program.cs
+++ ConditionTrue/Program.cs
@@ -6,6 +6,11 @@ namespace ConditionTrue
     {
         static void Main(string[] args)
         {
+            
+        }
+
+        static bool ConditionTrue(int i, int j)
+        {
             if (i == 1 || j == 1 || i + j == 1)
                 return true;
             return false;
---
+++ MaxProfil/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaxProfil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // [-1,9,0,8,-5,6,-24] => [1,5]
+            int[] arr = { -1, 9, 0, 8, -5, 6, -24 }; 
+            var res = MaxProfil(arr);
+            Console.WriteLine("Hello World!");
+        }
+
+        static int [] MaxProfil(int[] data)
+        {
+            List<(int, int[])> rapport = new List<(int, int[])>();
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                int calc = data[i];
+
+                for(int j = i+1 ; j < data.Length; j++)
+                {
+                    calc += data[j];
+                    rapport.Add((calc,new int[2] {i,j}));
+                }      
+            }
+
+            var res = rapport.OrderByDescending(x => x.Item1).FirstOrDefault().Item2;
+
+            return res;
+        }
+    }
+}
