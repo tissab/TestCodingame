25ad03c147de4296e446c90d9644e9faf707432d
Bassit Diby <josuebassit@gmail.com>
2023-03-30 01:30:53 +0200

    RebuildMessage

---
+++ GetPower_Iteration/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GetPower_Iteration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(int power in GetPower(2))
+            {
+                Console.WriteLine(power);
+                if(power> 20)
+                {
+                    break;
+                }
+            }
+        }
+
+        static IEnumerable<ulong>GetPower(uint value)
+        {
+            uint res = 1;
+            while (true)
+            {
+                yield return res *= value;
+            }
+        }
+    }
+}
--- RebuildMessage/Program.cs
+++ RebuildMessage/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -10,43 +11,88 @@ namespace RebuildMessage
     {
         static void Main(string[] args)
         {
-            //Solution(new string[] { "Ab", "bc","cd","efg", "xyZ" });
-            var res = Solution(new string[] { "*====#", "X-+-+-+-+-+-Z","#______X","A.........*" });
-            Console.WriteLine("Hello World!");
+            //Solution(new string[] { "defgx","bc","xyZ","cd","Ab" });
+            //"#______X", "A.........*", "*====#", "X-+-+-+-+-+-Z"
+            Stopwatch sw = Stopwatch.StartNew();
+            Stopwatch sw1 = Stopwatch.StartNew();
+
+            sw1.Start();
+            var res1 = Solution_1(new string[] { "defgx", "bc", "xyZ", "cd", "Ab" });
+            sw1.Stop();
+
+            sw.Start();
+            var res = Solution(new string[] { "defgx", "bc", "xyZ", "cd", "Ab" });
+            sw.Stop();
+
+            Console.WriteLine($"First  {res1} {sw.Elapsed.TotalMilliseconds} and second {res} {sw1.Elapsed.TotalMilliseconds}");
         }
 
         private static string Solution(string[] parts)
         {
-            var strStart = parts.Where(x => x.StartsWith('A')).FirstOrDefault();
-            var strEnd = parts.Where(x => x.EndsWith('Z')).FirstOrDefault();
+           StringBuilder sb = new StringBuilder();
 
-            var strArray = parts.Where(x => !x.Equals(strStart) && !x.Equals(strEnd)).ToList();
+            Queue<string> queue = new Queue<string>(parts);
 
-            List<string> result = new List<string> { strStart};
-
-            strArray.Add(strEnd);
-
-            foreach (var str in strArray)
+            while (queue.Count > 0)
             {
 
-                var end = result[^1][^1];
-                var start = str[0];
-                if (start == end)
+                if (sb.Length == 0)
                 {
-                    var c = str[1..];
-                    result.Add(c);
+                    sb.Append(queue.Dequeue());
                 }
                 else
                 {
-                    result.Add(str);
+                    var start = sb[0];
+                    var end   = sb[^1];
+
+                    var item = queue.Dequeue();
+
+                    if(start == item[^1] || end == item[0])
+                    {
+                        if(start == item[^1])
+                        {
+                            var res = item[0..^1];
+                            sb.Insert(0, res);
+                        }
+                        else
+                        {
+                            var res = item[1..^0];
+                            sb.Append(res);
+                        }
+                    }
+                    else
+                    {
+                        queue.Enqueue(item);
+                    }
+
                 }
             }
-           
-
-            return string.Join("", result); ;
-       
+            return string.Join("", sb); ;
         }
+
+       
+        private static string Solution_1(string[] args)
+        {
+            string message = args.Where(arg => arg[0] == 'A').FirstOrDefault();
+            char lastCharacter = message[message.Length - 1];
+
+            while (lastCharacter != 'z' && lastCharacter != 'Z')
+            {
+                char finalLastCharacter = lastCharacter;
+                string nextString = args.Where(arg => arg[0] == finalLastCharacter).FirstOrDefault();
+                lastCharacter = nextString[nextString.Length - 1];
+                message = message + nextString.Substring(1);
+            }
+
+            return message;
+        }
+
+
+
+
+
+
     }
 
-   
+
 }
