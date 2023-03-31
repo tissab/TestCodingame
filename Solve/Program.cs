97e355f85e7f6dcb6c68f8c18efc009c642a82ab
HermannKIPRE <115983115+HermannKIPRE@users.noreply.github.com>
2023-03-31 17:08:31 +0200

    MAJ Solve 2
    Ajout de la 2nd fonction de solve

--- Solve/Program.cs
+++ Solve/Program.cs
@@ -30,5 +30,55 @@ class Calc
     }
 
 
+    //By Hermann KIPRE
+    public static string Solve(int width, int height, int length, int mass) {
+	if(mass < 10 || mass> 1000)
+	{
+	throw new Exception ("Invalid mass");
+	}
+
+	if(width <10 || width > 1000)
+	{
+	throw new Exception ("Invalid width");
+	}
+	if(height <10 || height > 1000)
+	{
+	throw new Exception ("Invalid height");
+	}
+
+	if(length <10 || length > 1000)
+	{
+	throw new Exception ("Invalid length");
+	}
+
+	int encombrant = width * heigth * length;
+
+	string strMessageEncombrant = string.Empty;
+	string strMessageLourd = string.Empty;
+
+	if (encombrant >= 1000000 || (width >= 150 || height >= 150 || length >= 150)
+	{
+		strMessageEncombrant = "Encombrant";
+	}
+	if (mass >= 20)
+	{
+		strMessageLourd = "Lourd";
+	}
+
+	if(strMessageEncombrant != "Encombrant" && strMessageLourd != "Lourd")
+	{
+		return "STANDARD"
+	}
+	if(strMessageEncombrant != "Encombrant" && strMessageLourd != "Lourd")
+	{
+		return "REJECTED"
+	}
+	if(strMessageEncombrant != "Encombrant" && strMessageLourd != "Lourd")
+	{
+		return "SPECIAL"
+	}
+
+	return "";
+}
 
 }
